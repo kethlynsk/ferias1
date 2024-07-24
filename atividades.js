95b3f96a44ffa117890d3e84ec348a48374ef1bf
Kethlyn <kethlynsk17@outlook.com>
2024-07-23 23:16:11 -0300

    criar

---
+++ atividades.js
@@ -0,0 +1,31 @@
+const prompt = require("prompt-sync")();
+
+const atividades = [];
+
+const modelo = () => {    // uma função
+  let atividade = {       // com um objeto
+    dia: undefined,       // com a data que o usuario colocar
+    descricao: [],        // e um array para a descrição das atividades
+  };
+
+  atividade.dia = prompt("Qual dia? ");        // coloca a data na variavel dia do objeto atividade
+  
+  while (true) {  //enquanto tiver atividade para adcm vai rodar esse programa
+    let resposta = prompt("O que fez nesse dia? ");
+
+    if (resposta == "acabou") break;
+
+    atividade.descricao.push(resposta);       //variavel descricao, do objeto atividade, recebe a resposta do usuarioa
+  }
+
+  return atividade;                           //retorna para o objeto
+};
+
+
+const criar = () => {
+    let atividade = modelo();
+  
+    atividades.push(atividade);              // o array atividades recebe o objeto atividade
+  
+    console.log("Atividade criada");
+  };
