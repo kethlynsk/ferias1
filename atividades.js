2854e86e3c3a29c7038b450665df645c0ae6c88a
Kethlyn <kethlynsk17@outlook.com>
2024-07-23 23:20:35 -0300

    finaalizado

--- atividades.js
+++ atividades.js
@@ -29,3 +29,39 @@ const criar = () => {
   
     console.log("Atividade criada");
   };
+
+  const listar = () => {
+    atividades.forEach((atividade, indice) => {
+      console.log(`${indice + 1}. ${atividade.dia}: `);
+      atividade.descricao.forEach((descricao) => console.log(`- ${descricao}`));
+    });
+  };
+
+  const atualizar = () => {
+    listar();
+  
+    let indice = prompt("Qual indice deseja atualizar? ");
+  
+    let atividade = modelo();
+  
+    atividades[--indice] = atividade;
+  
+    console.log("Atualizado");
+  };
+  
+  const remover = () => {
+    listar();
+  
+    let indice = prompt("Qual indice sera removido? ");
+  
+    atividades.splice(--indice, 1);
+  
+    console.log("Removido");
+  };
+  
+  module.exports = {
+    criar,
+    atualizar,
+    remover,
+    listar,
+  };
